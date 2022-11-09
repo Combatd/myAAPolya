a88b6749087dcc5d5dd3c2e500b3c678f737b282
Combatd <combatdstar@gmail.com>
2022-11-08 16:36:19 -0800

    Set up Psuedocode

---
+++ moveZeroes.js
@@ -0,0 +1,25 @@
+/*
+Given an array nums, write a function to move all 0's to the end of it while
+maintaining the relative order of the non-zero elements.
+
+Input: [0,1,0,3,12] Output: [1,3,12,0,0]
+
+* You must do this in-place without making a copy of the array.
+* Minimize the total number of operations.
+*/
+
+/*
+1. Set a pointer to the last element in the array.
+2. Walk through each element of the array.
+3. If the element is zero, swap its position with the element at the pointer,
+   then decrement the pointer.
+*/
+
+function moveZeroes(nums) {
+  // 1. Set a pointer to the last element in the array.
+
+  // 2. Walk through each element of the array.
+
+  // 3. If the element is zero, swap its position with the element at the pointer,
+  // then decrement the pointer.
+}

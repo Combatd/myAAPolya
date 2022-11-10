a1425fd01e36ba527f6f295cc990ed30a5b1e420
Combatd <combatdstar@gmail.com>
2022-11-10 06:40:12 -0800

    Define #moveZeroes

--- moveZeroes.js
+++ moveZeroes.js
@@ -17,9 +17,23 @@ Input: [0,1,0,3,12] Output: [1,3,12,0,0]
 
 function moveZeroes(nums) {
   // 1. Set a pointer to the last element in the array.
+  let last = nums.length - 1;
 
   // 2. Walk through each element of the array.
+  for (let i = 0 ; i < last ; i++) {
 
-  // 3. If the element is zero, swap its position with the element at the pointer,
-  // then decrement the pointer.
+      // 3. If the element is zero,
+      if (nums[i] == 0) {
+          // Remove the zero at i,
+          nums.splice(i, 1);
+
+          // push it onto the end of the array
+          nums.push(0);
+
+          // then decrement the pointer.
+          last--;
+      }
+  }
+
+  return nums;
 }
